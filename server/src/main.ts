34c196b4e8ede45a8a8c21c1797ecc859ef32f49
davinzifc <47566127+davinzifc@users.noreply.github.com>
2024-04-03 12:04:41 -0500

    :sparkles: feat(shared): global utilities were configured

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,30 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import { env } from 'process';
 
 async function bootstrap() {
+  const logger: Logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Alliance research indicator management system API')
+    .setDescription('The Alliance research indicator management system API')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
+  const port: number = parseInt(env.PORT);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app
+    .listen(port)
+    .then(() => {
+      logger.debug(`Application is running http://localhost:${port}`);
+    })
+    .catch((err) => {
+      const portValue: number | string = port || '<Not defined>';
+      logger.error(`Application failed to start on port ${portValue}`);
+      logger.error(err);
+    });
 }
 bootstrap();
