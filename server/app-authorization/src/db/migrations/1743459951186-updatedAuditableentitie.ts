8b0a145852cd2ce55188546468a52dc6c8a172e7
davinzifc <47566127+davinzifc@users.noreply.github.com>
2025-08-26 19:07:59 -0500

    ✨ feat(users): Implement UserRepository and integrate into UsersModule and UsersService

--- server/app-authorization/src/db/migrations/1743459951186-updatedAuditableentitie.ts
+++ server/app-authorization/src/db/migrations/1743459951186-updatedAuditableentitie.ts
@@ -1,44 +1,109 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UpdatedAuditableentitie1743459951186 implements MigrationInterface {
-    name = 'UpdatedAuditableentitie1743459951186'
+export class UpdatedAuditableentitie1743459951186
+  implements MigrationInterface
+{
+  name = 'UpdatedAuditableentitie1743459951186';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`sec_refresh_tokens\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_role_focus\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_element_types\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_view_configurations\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_role_functional_permissions\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_endpoint_permissions\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_role_endpoint_permissions\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_user_role_contracts\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_roles\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_user_roles\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`user_status\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_users\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_user_role_results\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_entity_types\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_organizational_entities\` ADD \`deleted_at\` timestamp NULL`);
-        await queryRunner.query(`ALTER TABLE \`sec_template\` ADD \`deleted_at\` timestamp NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`sec_refresh_tokens\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_role_focus\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_element_types\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_view_configurations\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_role_functional_permissions\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_endpoint_permissions\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_role_endpoint_permissions\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_user_role_contracts\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_roles\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_user_roles\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_status\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_users\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_user_role_results\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_entity_types\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_organizational_entities\` ADD \`deleted_at\` timestamp NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_template\` ADD \`deleted_at\` timestamp NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`sec_template\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_organizational_entities\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_entity_types\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_user_role_results\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_users\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`user_status\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_user_roles\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_roles\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_user_role_contracts\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_role_endpoint_permissions\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_endpoint_permissions\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_role_functional_permissions\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_view_configurations\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_element_types\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_role_focus\` DROP COLUMN \`deleted_at\``);
-        await queryRunner.query(`ALTER TABLE \`sec_refresh_tokens\` DROP COLUMN \`deleted_at\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`sec_template\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_organizational_entities\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_entity_types\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_user_role_results\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_users\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`user_status\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_user_roles\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_roles\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_user_role_contracts\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_role_endpoint_permissions\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_endpoint_permissions\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_role_functional_permissions\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_view_configurations\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_element_types\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_role_focus\` DROP COLUMN \`deleted_at\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`sec_refresh_tokens\` DROP COLUMN \`deleted_at\``,
+    );
+  }
 }
---
+++ server/app-authorization/src/domain/entities/users/repositories/users.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { EntityManager, Repository } from 'typeorm';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private readonly entityManager: EntityManager) {
+    super(User, entityManager);
+  }
+
+  async findUserAndRoles(userId: number) {
+    const query = `select 
+          su.sec_user_id, 
+          su.first_name, 
+          su.last_name, 
+          su.email,
+          JSON_ARRAYAGG(sur.role_id) as roles
+      from sec_users su
+      left join sec_user_roles sur on sur.user_id = su.sec_user_id 
+      where su.sec_user_id = ?
+          and su.is_active = true
+      group by su.sec_user_id
+      limit 1;`;
+    return this.entityManager
+      .query(query, [userId])
+      .then((res) => res?.[0] ?? null);
+  }
+}
